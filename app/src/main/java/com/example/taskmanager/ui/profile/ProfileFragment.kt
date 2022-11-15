08dc81a7154d617c5494f5f17cf443fc9be03c05
islam0412 <islamlivaza2@gmail.com>
2022-11-15 11:34:03 +0300

    все

--- app/src/main/java/com/example/taskmanager/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/taskmanager/ui/profile/ProfileFragment.kt
@@ -1,22 +1,57 @@
 package com.example.taskmanager.ui.profile
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.taskmanager.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
-    private lateinit var binding:FragmentProfileBinding
+    private lateinit var binding: FragmentProfileBinding
+
+    private var imageUri: Uri? = null
+
+
+    companion object {
+        const val CODE_FOR_INTENT = 2006
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentProfileBinding.inflate(inflater,container,false)
+    ): View {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.profileImage.setOnClickListener {
+            preopenImageGalery()
+        }
+
 
     }
 
+    private fun preopenImageGalery() {
+        Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI).apply {
+            type = "image/*"
+            startActivityForResult(this, CODE_FOR_INTENT)
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && requestCode == CODE_FOR_INTENT) {
+            imageUri = data?.data
+            binding.profileImage.setImageURI(imageUri)
+        }
+    }
 }
